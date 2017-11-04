2cd0a70ef0adb2ed29f47a97c7a9d94b3aee3aae
Randi Lovelett <rlovelett@css.edu>
2017-11-04 16:55:41 -0500

    Request 407 - Created new URLTradeDataProvider class and implemented it in main program.

---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+using SingleResponsibilityPrinciple.Contracts;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider : ITradeDataProvider
+    {
+        public URLTradeDataProvider(String url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> GetURLTradeData(String url)
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
+        private readonly string url;
+
+    }
+}

dc410bebe26366dcb6d6acc829b633752ca5a3dd
Randi Lovelett <rlovelett@css.edu>
2017-11-04 16:40:21 -0500

    Request 405 - Made changes to ConsoleLogger to write Warnings and Information to an external log file.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using SingleResponsibilityPrinciple.Contracts;
 
@@ -8,11 +9,19 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>WARN</type><message>" + message + "</message></log> ", args);
+            }
             Console.WriteLine(string.Concat("WARN: ", message), args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>INFO</type><message>" + message + "</message></log> ", args);
+            }
             Console.WriteLine(string.Concat("INFO: ", message), args);
         }
     }
